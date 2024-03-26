bf24e30232adace98bc8b618d740d16ca3d915f4
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-03-26 12:20:37 +0530

    bcrypt

--- Server/models/user_models.js
+++ Server/models/user_models.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, require: true },
@@ -6,6 +7,22 @@ const userSchema = new mongoose.Schema({
   password: { type: String, require: true },
   phone: { type: String, require: true },
   isAdmin: { type: Boolean, default: false },
+});
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    next();
+  }
+
+  try {
+    const saltRound = await bcrypt.genSalt(5);
+
+    const hassPassword = await bcrypt.hash(user.password, saltRound);
+    user.password = hassPassword;
+  } catch (error) {
+    next(error);
+  }
 });
 
 const UserModel = new mongoose.model("User", userSchema);
